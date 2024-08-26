aab4a6e5c573be9d301fbdf73d14d42b355ac1d0
TillTerran <80480262+TillTerran@users.noreply.github.com>
2024-08-26 19:36:20 +0200

    making things more organised

--- level_transition.gd
+++ level_transition.gd
@@ -2,6 +2,8 @@ extends Node
 
 @onready var animation_player = $AnimationPlayer
 
+var previous_scene = ""
+
 func fade_from_black():
 	animation_player.play("fade_from_black")
 	await animation_player.animation_finished
@@ -11,4 +13,5 @@ func fade_from_black():
 
 func fade_to_black():
 	animation_player.play("fade_to_black")
+	previous_scene = get_tree().current_scene.scene_file_path
 	await animation_player.animation_finished
--- scripts/Events.gd
+++ scripts/Events.gd
@@ -1,13 +1,6 @@
 extends Node
 
 signal main_menu
-signal up_level
-signal down_level
-signal right_level
-signal left_level
-signal door1
-signal door2
-signal door3
-signal door4
+
 
 signal change_scene
